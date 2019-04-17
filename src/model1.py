fdd06bc3009caa95d63d01a8c2aa1d33aaf24ef1
JOHN REYNOLDS <jrr@panix.com>
2019-04-17 17:21:44 -0400

    model0 expanded with iterations

---
+++ src/model1.py
@@ -0,0 +1,105 @@
+import os
+import sys
+import argparse
+import numpy as np
+import pandas as pd
+import datetime
+import bisect
+import re
+from clean import simple_clean
+from clean import complex_clean
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+import matplotlib.pyplot as plt
+
+
+def decisionDF(decisionFile):
+    names = ["minutes_date","publish_date","before","after","decision","flag","change"]
+    usecols = [0,1,2,3,4,5,6]
+    dtypes={"minutes_date":'str',"publish_date":'str',"before":'float',"after":'float',
+            "decision":'str',"flag":'float',"change":'float'}
+    df = pd.read_csv(decisionFile, 
+                     usecols=usecols,
+                     header=None, 
+                     names=names,
+                     dtype=dtypes,
+                     sep=",")
+    df['minutes_date'] = pd.to_datetime(df['minutes_date'],format="%Y%m%d")
+    df['publish_date'] = pd.to_datetime(df['publish_date'],format="%Y%m%d")
+    return df
+
+def getMinutes(minutesDir, decisionDF, clean_algo=complex_clean):
+    minutes, publish , data = [], [], []
+    for files in os.listdir(minutesDir):
+        f, ext = os.path.splitext(files)
+        minutes.append(datetime.datetime.strptime(f.split("_")[0],"%Y%m%d"))
+        publish.append(datetime.datetime.strptime(f.split("_")[-1],"%Y%m%d"))
+        try:
+            text = clean_algo(open(os.path.join(minutesDir, files)).read().strip())
+            dec = df[df["publish_date"] == publish[-1]].iloc[0]["flag"]
+            data.append([text, dec])
+        except Exception as e:
+            print("exception reading file %s" % files)
+            print(e)
+            quit()
+    return data
+
+def splitTrainTest(data, trainPct):
+    np.random.shuffle(data)
+    Ntrain = int(len(data)*args.pctTrain)
+    train_data = pd.DataFrame(data[0:Ntrain],columns=['text', 'sentiment'])
+    test_data = pd.DataFrame(data[Ntrain:], columns=['text', 'sentiment'])
+    return train_data, test_data
+
+def getFeatures(train_data, test_data):
+    vectorizer = CountVectorizer(stop_words="english",preprocessor=None)
+    training_features = vectorizer.fit_transform(train_data["text"])                                 
+    test_features = vectorizer.transform(test_data["text"])
+    return training_features, test_features
+
+def runModels(models, data, Nitr, pctTrain):
+    results=[]
+    for iter in range(Nitr):
+        train_data, test_data = splitTrainTest(data, pctTrain)
+        training_features, test_features = getFeatures(train_data, test_data)
+        for i, m in enumerate(models):
+            model=m[1]
+            model.fit(training_features, train_data["sentiment"])
+            y_pred = model.predict(test_features)
+            acc = accuracy_score(test_data["sentiment"], y_pred)
+            if iter == 0:
+                results.append(np.zeros(Nitr))
+            results[i][iter]=acc
+    print("%-20s %4s %10s %10s" % ("Model Name", "Nitr", "mean(acc)", "std(acc)"))
+    for m, r in zip(models, results):
+        name, mu,r = m[0], np.mean(r), np.std(r)
+        print("%-20s %4d %10.4f %10.4f" % (name, Nitr, mu, r))
+                
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='SocialNetworks CSV to HDF5')
+    parser.add_argument('--decision', default="../text/history/RatesDecision.csv")
+    parser.add_argument('--minutes', default="../text/minutes")
+    parser.add_argument('--speeches', default="../text/speeches")
+    parser.add_argument('--statements', default="../text/statements")
+    parser.add_argument('--pctTrain', default=0.75, type=float)
+    parser.add_argument('--Niter', default=10, type=int)
+    args = parser.parse_args()
+
+    df = decisionDF(args.decision)
+    data = getMinutes(args.minutes, df)
+
+    # Train on current minutes
+    models=[("svm",LinearSVC()),
+            ("logistic",LogisticRegression()),
+            ("logistic_lasso",LogisticRegression(penalty='l1'))]
+
+
+    runModels(models, data, args.Niter, args.pctTrain)
+
+
+
+
+
