37c77dd2e20524e70edc4ff9a9e03b43d8e9070d
jrrpanix <john@Johns-iMac-2.fios-router.home>
2019-04-23 10:57:00 -0400

    update model3

--- src/model3_neuralNet.py
+++ src/model3_neuralNet.py
@@ -54,17 +54,43 @@ if __name__ == '__main__':
     parser.add_argument('--speeches', default="../text/speeches")
     parser.add_argument('--statements', default="../text/statements")
     parser.add_argument('--pctTrain', default=0.75, type=float)
-    parser.add_argument('--Niter', default=10, type=int)
+    parser.add_argument('--Niter', help="number of times to refit data", default=10, type=int)
     parser.add_argument('--cleanAlgo', default="complex")
     parser.add_argument('--ngram', nargs='+', default=['1,1'])
+    parser.add_argument('--max_iter', help="max iterations for sklearn solver", default=25000, type=int)
+    parser.add_argument('--solver', help="solver for sklearn algo", default='liblinear')
+    parser.add_argument('--data', nargs="+", default=["minutes", "speeches", "statements"])
     args = parser.parse_args()
 
-    
+    ngrams = [(int(x.split(",")[0]),int(x.split(",")[1])) for x in args.ngram]
     clean_algo = complex_clean if args.cleanAlgo == "complex" else simple_clean
-    df = modelutils.decisionDF(args.decision)
-    data, publish = modelutils.getMinutes(args.minutes, df, clean_algo)
-    #data_statements = modelutils.getStatements(args.statements, df, clean_algo)
+    pctTrain, cleanA, Niter, ngram = args.pctTrain, args.cleanAlgo, args.Niter, args.ngram
+    solver, max_iter, datasetlist = args.solver, args.max_iter, args.data
 
+    assert len(datasetlist) > 0, "no data sets specified"
+    datasetlabel=":".join(d for d in datasetlist)
+
+    # fed rate decison matrix
+    df = modelutils.decisionDF(args.decision)
+
+    # datsets
+    data_set, N, start, end = [], 0, datetime.datetime.now(), datetime.datetime(1970, 1, 1)
+    if "minutes" in datasetlist:
+        data_set.append(modelutils.getMinutes(args.minutes, df, clean_algo))
+        (N, start, end) = modelutils.getBounds(data_set[-1])
+    if "speeches" in datasetlist:
+        data_set.append(modelutils.getSpeeches(args.speeches, df, clean_algo))
+        (N1, start1, end1) = modelutils.getBounds(data_set[-1])
+        N , start, end = N + N1, min(start, start1), max(end, end1)
+    if "statements" in datasetlist:
+        data_set.append(modelutils.getStatements(args.statements, df, clean_algo))
+        (N1, start1, end1) = modelutils.getBounds(data_set[-1])
+        N , start, end = N + N1, min(start, start1), max(end, end1)
+
+    assert N  > 0, "no data in data_set"
+    start, end = start.strftime("%m/%d/%Y"), end.strftime("%m/%d/%Y")
+
+    
     net = FCNet1(100)
 
 
