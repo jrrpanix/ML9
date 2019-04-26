08b67e76fa7a521090d46f04f9227e15bec204ac
jeremylao <jeremylao12@yahoo.co.uk>
2019-04-25 22:49:04 -0400

    update; ;

--- src/model2-lda.py
+++ src/model2-lda.py
@@ -18,6 +18,8 @@ import matplotlib.pyplot as plt
 import csv
 from modelutils import modelutils
 from sklearn.metrics import precision_recall_fscore_support
+from sklearn import decomposition
+from sklearn.decomposition import LatentDirichletAllocation
 
 # this version allows for multiple fit methods (SVM, Logistic, Logistic Lasso)
 # this method also computes the mean and std deviation of the accuracy by
@@ -27,31 +29,29 @@ from sklearn.metrics import precision_recall_fscore_support
 
 # python ./model2_0.py --ngram 1,1 2,2 3,3 1,3 2,3 --Niter 3 --pctTrain .8 --data minutes speeches statements
 
+def print_top_words(model, feature_names, n_top_words):
+    print()
+    for topic_idx, topic in enumerate(model.components_):
+        message = "Topic #%d: " % topic_idx
+        message += "|".join([feature_names[i]
+                             for i in topic.argsort()[:-n_top_words - 1:-1]])
+        print(message)
+        print()
+    print()
 
-def runModels(models, model_data_set, Nitr, pctTrain, ngram):
-    results, prec,recall,f1, trainPos, testPos=[],[],[],[], np.zeros(Nitr), np.zeros(Nitr)
-    for iter in range(Nitr):
-        train_data, test_data = modelutils.splitTrainTest(model_data_set, pctTrain)
-        training_features, test_features = modelutils.getFeatures(train_data, test_data, ngram)
-        for i, m in enumerate(models):
-            model=m[1]
-            model.fit(training_features, train_data["ActionFlag"])
-            y_pred = model.predict(test_features)
-            acc = accuracy_score(test_data["ActionFlag"], y_pred)
-            if iter == 0:
-                results.append(np.zeros(Nitr))
-                prec.append(np.zeros(Nitr))
-                recall.append(np.zeros(Nitr))
-                f1.append(np.zeros(Nitr))
-            results[i][iter]=acc
-            prec_recall = precision_recall_fscore_support(test_data['ActionFlag'].tolist(), y_pred, average='binary') 
-            prec[i][iter] = prec_recall[0]
-            recall[i][iter] = prec_recall[1]
-            f1[i][iter] = prec_recall[2]
-        trainPos[iter] = train_data["ActionFlag"].sum()/ len(train_data)
-        testPos[iter] = test_data["ActionFlag"].sum()/ len(test_data)
-    return results, trainPos, testPos, prec, recall, f1
-                
+def runModels(model_data_set):
+    docs = pd.concat(model_data_set)
+    docs = docs["DocText"]
+
+    vectorizer = CountVectorizer(min_df = 5,
+      stop_words='english',
+      ngram_range=(1,2),
+      lowercase=True)        
+    wordMatrix = vectorizer.fit_transform(docs)
+    lda = LatentDirichletAllocation(n_components=4,learning_method="online",max_iter=50)
+    lda.fit(wordMatrix)
+    tf_feature_names = vectorizer.get_feature_names()
+    print_top_words(lda, tf_feature_names, 15)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='SocialNetworks CSV to HDF5')
@@ -92,33 +92,6 @@ if __name__ == '__main__':
         data_set.append(modelutils.getStatements(args.statements, df, clean_algo))
         (N1, start1, end1) = modelutils.getBounds(data_set[-1])
         N , start, end = N + N1, min(start, start1), max(end, end1)
-
     assert N  > 0, "no data in data_set"
     start, end = start.strftime("%m/%d/%Y"), end.strftime("%m/%d/%Y")
-    
-
-    # Train on current minutes
-    models=[("svm",LinearSVC(max_iter=max_iter)),
-            ("logistic",LogisticRegression(solver=solver,max_iter=max_iter)),
-            ("logistic_lasso",LogisticRegression(penalty='l1',solver=solver,max_iter=max_iter)),
-            ("Naive Bayes",MultinomialNB())]
-
-    print("Determining Fed Action from minutes")
-    print("%-20s %5s %5s %10s %10s %5s %8s %7s %10s %10s %-27s %6s %6s %6s %6s %6s" % 
-          ("Model Name", "NGram", "Niter", "mean(acc)", "std(acc)","N","PctTrain", "clean", "start", "end", "Data Sets", "TrainP", "TestP", "Prec", "Recall", "F1"))
-
-    outputDF = []
-
-    for ngram in ngrams:
-        results, trainPos, testPos, prec, recall, f1 = runModels(models, data_set, Niter, pctTrain, ngram)
-        ngramstr = str(ngram[0]) + ":" + str(ngram[1])
-        for m, r, t, u, v in zip(models, results, prec, recall, f1):
-            name, mu, s, precMu, recallMu, f1Mu = m[0], np.mean(r), np.std(r), np.mean(t), np.mean(u), np.mean(v)
-            print("%-20s %5s %5s %10.4f %10.4f %5d %8.3f %7s %10s %10s %-27s %6.3f %6.3f  %6.3f %6.3f  %6.3f " % 
-                  (name, ngramstr, Niter, mu, s, N, pctTrain, cleanA, start, end, datasetlabel, np.mean(trainPos), np.mean(testPos), precMu, recallMu, f1Mu))
-
-            outputDF.append([name, ngramstr, Niter, mu, s, N, pctTrain, cleanA, start, end, datasetlabel, np.mean(trainPos), np.mean(testPos), precMu, recallMu, f1Mu])
-        print("")
-
-    outputDF = pd.DataFrame(outputDF)
-    outputDF.to_csv('../text/data_for_graphs/model2_anagrams.csv', index=False, header=["Model Name", "NGram", "Niter", "mean(acc)", "std(acc)","N","PctTrain", "clean", "start", "end", "Data Sets", "TrainP", "TestP", "Prec", "Recall", "F1"])
+    runModels(data_set)
