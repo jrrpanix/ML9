9425f626a01687a45ac8fa787bf454339b355aae
jrrpanix <jreynoldsvc@gmail.com>
2019-05-11 09:24:38 -0400

    more changes to model_analysis

--- src/model_anlysis.py
+++ src/model_anlysis.py
@@ -33,7 +33,7 @@ import scipy
 
 
 def runModels(models, model_data_set, Nitr, pctTrain, ngram, tfid):
-    results, prec,recall,f1, trainPos, testPos=[],[],[],[], np.zeros(Nitr), np.zeros(Nitr)
+    results, prec,recall,f1, nonZeroC, trainPos, testPos=[],[],[],[], [], np.zeros(Nitr), np.zeros(Nitr)
     for iter in range(Nitr):
         train_data, test_data = modelutils.splitTrainTest(model_data_set, pctTrain)
         training_features, test_features = modelutils.getFeatures(train_data, test_data, ngram)
@@ -46,6 +46,8 @@ def runModels(models, model_data_set, Nitr, pctTrain, ngram, tfid):
         for i, m in enumerate(models):
             model=m[1]
             model.fit(training_features, train_data["ActionFlag"])
+            C = model.coef_
+            gtZ = len(C[np.abs(C[:]) > 1e-5])
             y_pred = model.predict(test_features)
             acc = accuracy_score(test_data["ActionFlag"], y_pred)
             if iter == 0:
@@ -53,14 +55,16 @@ def runModels(models, model_data_set, Nitr, pctTrain, ngram, tfid):
                 prec.append(np.zeros(Nitr))
                 recall.append(np.zeros(Nitr))
                 f1.append(np.zeros(Nitr))
+                nonZeroC.append(np.zeros(Nitr))
             results[i][iter]=acc
             prec_recall = precision_recall_fscore_support(test_data['ActionFlag'].tolist(), y_pred, average='binary') 
             prec[i][iter] = prec_recall[0]
             recall[i][iter] = prec_recall[1]
             f1[i][iter] = prec_recall[2]
+            nonZeroC[i][iter] = gtZ
         trainPos[iter] = train_data["ActionFlag"].sum()/ len(train_data)
         testPos[iter] = test_data["ActionFlag"].sum()/ len(test_data)
-    return results, trainPos, testPos, prec, recall, f1, NF, norm, nz , sz
+    return results, trainPos, testPos, prec, recall, f1, NF, norm, nz , sz, nonZeroC
                 
 
 if __name__ == '__main__':
@@ -79,6 +83,7 @@ if __name__ == '__main__':
     parser.add_argument('--stack', action='store_true', default=False)
     parser.add_argument('--slow', action='store_true', default=False)
     parser.add_argument('--tfid',action='store_true', default=False)
+    parser.add_argument('--nb',action='store_true', default=False)
     parser.add_argument('-o','--output', default='../output/data_for_graphs/model2_anagrams.csv')
     args = parser.parse_args()
 
@@ -131,55 +136,39 @@ if __name__ == '__main__':
     tfid= args.tfid
     tfidstr= "False" if tfid == False else "True"
 
-    
-
-    # Train on current minutes
-    models=[("svm",LinearSVC(max_iter=max_iter)),
-            ("logistic",LogisticRegression(solver=solver,max_iter=max_iter)),
-            ("logistic_lasso",LogisticRegression(penalty='l1',solver=solver,max_iter=max_iter)),
-            ("Naive Bayes",MultinomialNB())]
-            #("Naive Bayes",MultinomialNB())]
-
-    models=[
+    if args.nb :
+        models=[
             ("MultiNB0.0",MultinomialNB(alpha=1e-4)),
             ("MultiNB0.5",MultinomialNB(alpha=0.5)),
             ("MultiNB1.0",MultinomialNB(alpha=1.0)),
             ("MultiNB2.0",MultinomialNB(alpha=2.0)),
-            ("MultiNB10.0",MultinomialNB(alpha=10.0)),
-            ("MultiNB15.0",MultinomialNB(alpha=15.0)),
-            ("MultiNB20.0",MultinomialNB(alpha=20.0))
-            ]
-            #("Naive Bayes",MultinomialNB())]
-    models=[
-            ("MultiNB0.0",MultinomialNB(alpha=1e-4)),
-            ("MultiNB1.0",MultinomialNB(alpha=1.0)),
-            ("MultiNB15.0",MultinomialNB(alpha=15.0)),
-            ("Logistic_Lasso0.9",LogisticRegression(penalty='l1',solver=solver,max_iter=max_iter, C=0.9)),
-            ("Logistic Lasso2.0",LogisticRegression(penalty='l1',solver=solver,max_iter=max_iter, C=2.0))]
-#            ("Logistic",LogisticRegression(solver=solver,max_iter=max_iter))
-#            ]
+            ("MultiNB15.0",MultinomialNB(alpha=15.0))]
 
-
-
+    else:
+        models=[
+            ("Logistic Lasso0.3",LogisticRegression(penalty='l1',solver=solver,max_iter=max_iter, C=0.3)),
+            ("Logistic Lasso1.0",LogisticRegression(penalty='l1',solver=solver,max_iter=max_iter, C=1.0)),
+            ("Logistic Lasso2.0",LogisticRegression(penalty='l1',solver=solver,max_iter=max_iter, C=2.0)),
+            ("Logistic Lasso5.0",LogisticRegression(penalty='l1',solver=solver,max_iter=max_iter, C=5.0)),
+            ("Logistic",LogisticRegression(solver=solver,max_iter=max_iter))            ]
 
     print("Determining Fed Action from minutes")
-    print("%-20s %5s %5s %10s %10s %5s %8s %7s %10s %10s %-27s %6s %6s %6s %6s %6s %5s %5s %10s %8s %8s %8s %10s %6s" % 
-          ("Model Name", "NGram", "Niter", "mean(acc)", "std(acc)","N","PctTrain", "clean", "start", "end", "Data Sets", "TrainP", "TestP", "Prec", "Recall", "F1", "Stack", "Tfid","NF", "Norm", "Ratio", "Sparcity", "Size", "NZ"))
+    print("%-20s %5s %5s %10s %10s %5s %8s %7s %10s %10s %-27s %6s %6s %6s %6s %6s %5s %5s %10s %8s %8s %8s %10s %6s %8s" % 
+          ("Model Name", "NGram", "Niter", "mean(acc)", "std(acc)","N","PctTrain", "clean", "start", "end", "Data Sets", "TrainP", "TestP", "Prec", "Recall", "F1", "Stack", "Tfid","NF", "Norm", "Ratio", "Sparcity", "Size", "NZMtx", "PctNZC"))
 
     outputDF = []
 
     for ngram in ngrams:
-        results, trainPos, testPos, prec, recall, f1, NF, norm, nz, sz = runModels(models, data_set, Niter, pctTrain, ngram, tfid)
-        sparcityScore = nz/sz
+        results, trainPos, testPos, prec, recall, f1, NF, norm, nz, sz, nonZeroC = runModels(models, data_set, Niter, pctTrain, ngram, tfid)
+        sparcity = nz/sz
         ngramstr = str(ngram[0]) + ":" + str(ngram[1])
-        for m, r, t, u, v in zip(models, results, prec, recall, f1):
-            name, mu, s, precMu, recallMu, f1Mu , normR = m[0], np.mean(r), np.std(r), np.mean(t), np.mean(u), np.mean(v), norm/NF
-            print("%-20s %5s %5s %10.4f %10.4f %5d %8.3f %7s %10s %10s %-27s %6.3f %6.3f %6.3f %6.3f %6.3f %5s %5s %10.0f %8.0f %8.6f %8.6f %10.0f %6.0f " % 
-                  (name, ngramstr, Niter, mu, s, N, pctTrain, cleanA, start, end, datasetlabel, np.mean(trainPos), np.mean(testPos), precMu, recallMu, f1Mu, stack, tfidstr, NF, norm, normR, sparcityScore, sz, nz))
+        for m, r, t, u, v, coeff_nz in zip(models, results, prec, recall, f1, nonZeroC):
+            name, mu, s, precMu, recallMu, f1Mu , normR, NonZeroCoeff = m[0], np.mean(r), np.std(r), np.mean(t), np.mean(u), np.mean(v), norm/NF, np.mean(coeff_nz)/NF
+            print("%-20s %5s %5s %10.4f %10.4f %5d %8.3f %7s %10s %10s %-27s %6.3f %6.3f %6.3f %6.3f %6.3f %5s %5s %10.0f %8.0f %8.6f %8.6f %10.0f %6.0f %8.6f" % 
+                  (name, ngramstr, Niter, mu, s, N, pctTrain, cleanA, start, end, datasetlabel, np.mean(trainPos), np.mean(testPos), precMu, recallMu, f1Mu, stack, tfidstr, NF, norm, normR, sparcity, sz, nz, NonZeroCoeff))
 
-            outputDF.append([name, ngramstr, Niter, mu, s, N, pctTrain, cleanA, start, end, datasetlabel, np.mean(trainPos), np.mean(testPos), precMu, recallMu, f1Mu, stack])
-        print("")
+            outputDF.append([name, ngramstr, Niter, mu, s, N, pctTrain, cleanA, start, end, datasetlabel, np.mean(trainPos), np.mean(testPos), precMu, recallMu, f1Mu, stack, tfidstr, NF, norm, normR, sparcity, sz, nz, NonZeroCoeff])
 
     outputDF = pd.DataFrame(outputDF)
     print("writing to {}".format(args.output))
-    outputDF.to_csv(args.output, index=False, header=["Model Name", "NGram", "Niter", "mean(acc)", "std(acc)","N","PctTrain", "clean", "start", "end", "Data Sets", "TrainP", "TestP", "Prec", "Recall", "F1", "Stack"])
+    outputDF.to_csv(args.output, index=False, header=["Model Name", "NGram", "Niter", "mean(acc)", "std(acc)","N","PctTrain", "clean", "start", "end", "Data Sets", "TrainP", "TestP", "Prec", "Recall", "F1", "Stack", "Tfid", "NF", "norm", "normR", "sparcity", "sz", "nz", "NonZeroCoeff"])
